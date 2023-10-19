ad6fd495602e75c5cfa0860a793b09d992a26eac
example-git-student <100669349+Periyasedhu@users.noreply.github.com>
2023-10-19 21:42:54 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '56087bf8-79c4-407e-8fd9-53ddb0a8365c';
+export const MODAL_CLOSE_SEC = 2.5;
